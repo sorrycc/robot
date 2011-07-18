ed1c73211d25a8428d1c78114f0e1c36cd22b4e4
chencheng (yunqian) <sorrycc@gmail.com>
2011-07-18 10:45:48 +0800

    first commit

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+    twitter: {
+        consumer_key:        'R0deKuaE9nbvl22i7Qq8bw',
+        consumer_secret:     'wmvvw0b8bNqqME6OZqhEq9QIMrUjRDbfKgAZrD5ALo',
+        access_token:        '15205852-BYhywRfn2j0KeLgyHG0Lsxck4yXRUPDOjtTx3Y',
+        access_token_secret: 'GLwrGTs983RBiOEM7XTVCp5XEMogZ7x6EaHf8IU3B34'
+    }
+};
